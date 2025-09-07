318d191e93a0780bd5e693ac04efaf9db15660ce
Hiarlley Brandão <hiarlleybrandao@gmail.com>
2025-09-07 13:14:11 -0300

    Project created

---
+++ src/main/java/com/brandaoprojects/hblist/HblistApplication.java
@@ -0,0 +1,13 @@
+package com.brandaoprojects.hblist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HblistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HblistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandaoprojects/hblist/HblistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandaoprojects.hblist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HblistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
